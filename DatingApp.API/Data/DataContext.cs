7250c569268bc39bb9bbbecc9405e0ffa7e1fa78
Elvis Miguel Rocha Boza <erocha-as@avantica.net>
2019-02-15 15:50:12 -0600

    Section 16 lecture 165

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -11,10 +11,11 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
          protected override void OnModelCreating(ModelBuilder builder)
         {
-             builder.Entity<Like>()
+            builder.Entity<Like>()
                 .HasKey(k => new {k.LikerId, k.LikeeId});
 
             builder.Entity<Like>()
@@ -27,6 +28,16 @@ namespace DatingApp.API.Data
                 .HasOne(u => u.Liker)
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.MesagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(u => u.MessagesReceived)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
